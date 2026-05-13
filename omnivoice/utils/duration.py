804a8bf5100595863a9d7efa90cf0273192d725e
NguyenQuocKhanh0 <108500182+NguyenQuocKhanh0@users.noreply.github.com>
2026-05-13 10:19:29 +0700

    Update duration.py

--- omnivoice/utils/duration.py
+++ omnivoice/utils/duration.py
@@ -85,11 +85,11 @@ class RuleDurationEstimator:
 
         # Có phân biệt pause tốt hơn code cũ một chút.
         self.punctuation_weights = {
-            ",": 0.75,
+            ",": 1.75,
             "،": 0.35,
             ";": 0.45,
             ":": 0.45,
-            ".": 1.3,
+            ".": 2.5,
             "。": 0.65,
             "?": 0.75,
             "？": 0.75,
